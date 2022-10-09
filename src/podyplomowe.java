495ef3d471f780cdab7b24d40daf8f29761290a1
jckkupczyk <jckkupczyk@gmail.com>
2022-10-09 14:36:21 +0200

    Obwód prostokąta.

--- src/podyplomowe.java
+++ src/podyplomowe.java
@@ -4,5 +4,6 @@ public class podyplomowe {
         int b = 10;
 
         System.out.println("Pole prostokoąta jest równe: " + (a * b));
+        System.out.println("Obwód prostokoąta jest równy: " + (2*a + 2 * b));
     }
 }
