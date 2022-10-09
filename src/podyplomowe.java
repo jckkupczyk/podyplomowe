d9798c5feb52da6bb3497892c5b9688cdbaf549c
jckkupczyk <jckkupczyk@gmail.com>
2022-10-09 14:33:38 +0200

    Initial commit

---
+++ src/podyplomowe.java
@@ -0,0 +1,8 @@
+public class podyplomowe {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Pole prostokoąta jest równe: " + (a * b));
+    }
+}
